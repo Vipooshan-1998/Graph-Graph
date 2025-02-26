4fbb1fd316cadb029f6c4a3b4ef077732b596c71
Vipooshan Vipulananthan <77396714+Vipooshan-1998@users.noreply.github.com>
2025-02-26 10:52:21 +0530

    Update dataset_dota.py

--- dataset_dota.py
+++ dataset_dota.py
@@ -373,6 +373,7 @@ class CrossValDataset(Dataset):
             curr_toa = self.n_frames + 1
 
         # Reading frame (i3d) features for the frames
+        print('-------------------------------------------------------------------------------')
         if curr_vid_label > 0:
             img_file = os.path.join(self.img_dataset_path, "positive",
                                     feature_path.split('/')[-1].split(".")[0] + '.npy')
