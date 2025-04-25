986375dc4b794569cfbcb76bf2cc8d446d1031f2
Vipooshan Vipulananthan <77396714+Vipooshan-1998@users.noreply.github.com>
2025-04-25 13:52:04 +0530

    Update dataset_dota.py

--- dataset_dota.py
+++ dataset_dota.py
@@ -392,10 +392,10 @@ class CrossValDataset(Dataset):
         # logging.info("all_data[labels]: ", all_data['labels'])
         # logging.info('curr_vid_label: ', curr_vid_label)
         # logging.info('feature_path.split: ', feature_path.split('/'))
-        if "neg" in feature_path.split('/')[-1]:
-            curr_vid_label = 0
-        else:
-            curr_vid_label = 1
+        # if "neg" in feature_path.split('/')[-1]:
+        #     curr_vid_label = 0
+        # else:
+        #     curr_vid_label = 1
         if curr_vid_label > 0:
             curr_toa = self.get_toa_all(video_name)
         else:
