dd645c2c77b3cb878985c2db87085dcc9814f6ee
Vipooshan Vipulananthan <77396714+Vipooshan-1998@users.noreply.github.com>
2025-11-17 13:02:42 +0530

    Update cross_validate_dota.py

--- cross_validate_dota.py
+++ cross_validate_dota.py
@@ -378,9 +378,9 @@ if __name__ == "__main__":
 
         # Prepare CSV rows
         for f_name in train_files:
-            csv_rows.append({"file_name": f_name, "fold": fold+1}. "Train": 1, "Test":0)
+            csv_rows.append({"file_name": f_name, "fold": fold+1, "Train": 1, "Test":0})
         for f_name in test_files:
-            csv_rows.append({"file_name": f_name, "test_fold": fold+1}, "Train": 0, "Test":1)
+            csv_rows.append({"file_name": f_name, "test_fold": fold+1, "Train": 0, "Test":1})
 
     
         # Print fold info
